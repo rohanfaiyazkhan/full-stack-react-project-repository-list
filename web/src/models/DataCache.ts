03609a6933d6b23f7d1733bfe53b4894aebf9487
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-07-04 11:03:47 -0400

    Modified DataCache model to include loadingState and requestOn flag

--- web/src/models/DataCache.ts
+++ web/src/models/DataCache.ts
@@ -2,8 +2,10 @@ import { ApiError } from './ApiError';
 import { Repo } from './Repo';
 
 export interface DataCache<T = Repo[]> {
+  requestOn?: number;
   fetchedOn?: number;
   failedOn?: number;
   error?: ApiError;
   resource?: T;
+  loadingState?: string;
 }
