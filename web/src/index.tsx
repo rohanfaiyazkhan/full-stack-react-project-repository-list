db90fbcd1be6290674814951af838dcf09d8e771
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-07-03 22:21:36 -0400

    Setup TailwindCSS

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 import { reportWebVitals } from './reportWebVitals';
 
-import './index.css';
+import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
