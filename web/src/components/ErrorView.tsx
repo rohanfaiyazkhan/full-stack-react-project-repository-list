d12b887e4d7cd6869cf576e5ac4deea80b862cba
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-07-04 18:59:21 -0400

    Error banner component

---
+++ web/src/components/ErrorView.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ErrorIcon from '../icons/ErrorIcon';
+
+export function ErrorView(
+  props: React.PropsWithChildren<any>
+): React.ReactElement<any, any> {
+  return (
+    <div className="rounded shadow-sm flex w-full max-w-md mx-auto my-4">
+      <span className="bg-red-600 text-red-100 px-2 py-1 flex justify-center items-center">
+        <ErrorIcon className="w-8 h-8" />
+      </span>
+      <span className="flex-grow px-2 py-2 bg-red-300 text-red-900">
+        {props.children}
+      </span>
+    </div>
+  );
+}
