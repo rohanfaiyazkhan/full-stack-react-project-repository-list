fa10fc6b086146da5b205f55dbb620d8c44d0689
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-07-03 20:51:45 -0400

    Added data and time utils

---
+++ api/src/utils/dateTime.ts
@@ -0,0 +1,17 @@
+// Get current unix time in seconds
+export function getCurrentTimeStamp() {
+    return Math.floor(Date.now() / 1000);
+  }
+  
+  // Take a unix timestamp (in seconds) and return a date object
+  export function readUnixTimestamp(input: number) {
+    const timestampInMilliseconds = input * 1000;
+    const time = new Date(timestampInMilliseconds);
+  
+    return time;
+  }
+  
+  // Wrapper for date formatting. Although implementation is simple, there is room for more complex date formatting if needed
+  export function formatDate(input: Date) {
+    return input.toLocaleString('en-us');
+  }
