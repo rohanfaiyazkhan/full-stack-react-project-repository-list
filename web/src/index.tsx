d420460a15672ff87228ee03573898e2638bbd21
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-07-05 13:32:44 -0400

    Modified app launch to wait for mock server to start first

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,20 +1,30 @@
+import { SetupWorkerApi } from 'msw';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+let workerReady: Promise<{ worker: SetupWorkerApi }> | undefined;
+
+// This specifies the mock server should start in development mode
+// Comment/uncomment this block to disable/enable this functionality
 if (process.env.NODE_ENV === 'development') {
-  import('./mocks/browser')
-    .then(({ worker }) => {
-      worker.start();
-    })
-    .catch((err) => {
-      console.error('Failed to start mock service worker');
-    });
+  workerReady = import('./mocks/browser');
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+async function run() {
+  // If in development mode, wait till mock server starts before launcing app
+  // This allows us to work on UI without making a ton of requests
+  if (workerReady) {
+    const { worker } = await workerReady;
+    worker.start();
+  }
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+run();
