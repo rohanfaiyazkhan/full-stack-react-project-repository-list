733862c943bd52aa94703531a7990db5a0214c11
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-07-05 13:24:36 -0400

    Allow ErrorView component to inherit styles

--- web/src/components/ErrorView.tsx
+++ web/src/components/ErrorView.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import cx from 'classnames';
 import ErrorIcon from '../icons/ErrorIcon';
 
 export function ErrorView(
   props: React.PropsWithChildren<any>
 ): React.ReactElement<any, any> {
   return (
-    <div className="rounded shadow-sm flex w-full max-w-md mx-auto my-4">
+    <div
+      className={cx(
+        'rounded shadow-sm flex w-full max-w-md mx-auto my-4',
+        props.className
+      )}
+    >
       <span className="bg-red-600 text-red-100 px-2 py-1 flex justify-center items-center">
         <ErrorIcon className="w-8 h-8" />
       </span>
