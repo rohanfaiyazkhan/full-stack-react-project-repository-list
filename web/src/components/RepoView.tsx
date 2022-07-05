6654a2c0c84c1ef042686784ca6f9e1874d3b0c2
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2022-07-05 13:34:14 -0400

    View for single repository details expanded to include latest commit and README.md

--- web/src/components/RepoView.tsx
+++ web/src/components/RepoView.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { Repo } from '../models/Repo';
+import { LatestCommit } from './LatestCommit';
+import { ReadmeViewer } from './markdown/ReadmeViewer';
 
 interface RepoViewProps {
   repo: Repo;
   returnToRootHandler: () => void;
 }
 
-export function RepoView(props: RepoViewProps): React.ReactElement<any, any> {
-  return <div />;
+export function RepoView({
+  repo,
+  returnToRootHandler,
+}: RepoViewProps): React.ReactElement<any, any> {
+  return (
+    <div className="max-w-screen-lg mx-auto w-full">
+      <button
+        onClick={returnToRootHandler}
+        className="mb-2 px-4 py-1 bg-gray-300 shadow font-bold hover:bg-gray-400 transition-colors focus:text-blue-800 focus:underline"
+      >
+        Go Back to All Repositories
+      </button>
+      <h2 className="text-xl mb-4">
+        Repository Name: <span className="font-bold">{repo.name}</span>
+      </h2>
+      <LatestCommit id={repo.id} commitsUrl={repo.commits_url} />
+      <ReadmeViewer repoFullName={repo.full_name} />
+    </div>
+  );
 }
